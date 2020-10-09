4f83ddffe429c54e084a542bd0bf18931ff32696
Miguel Perez <miguel@miguelpz.com>
2020-10-08 22:11:38 -0400

    Fix issue with zip command

--- tools/release.sh
+++ tools/release.sh
@@ -3,4 +3,5 @@
 mkdir build/
 go build -o build/streaming-management main.go
 cp -r obs-assets/ build/obs-assets
-zip -r --junk-paths streaming-management-$RUNNER_OS build/
+cd build/
+zip -r ../streaming-management-$RUNNER_OS ./*
